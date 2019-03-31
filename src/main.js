0c10b5c3af73b7043db0f7eb2cba4e884f46ebd4
lcolok <425311101@qq.com>
2019-03-31 14:34:25 +0800

    增加vuex

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   i18n,
+  store,
   render: h => h(App)
 }).$mount('#app')
